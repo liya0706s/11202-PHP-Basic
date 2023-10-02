0daefad30d1d2c01ada8da42a6e8abe9e2ed8ef0
Angie <liya0706s@gmail.com>
2023-10-02 10:30:16 +0800

    PHP基礎-變數

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+// $age=30;
+// $name='李';
+
+// echo $age;
+// echo $name;
+
+// $age=$name;
+// $name=$age;
+
+// echo $age;
+// echo $name;
+
+$age=30;
+$name='李';
+
+// echo $age;
+// echo $name;
+
+$tmp=$age;
+$age=$name;
+$name=$tmp;
+
+echo $age;
+echo $name;
+
+?>
+</body>
+</html>
+
