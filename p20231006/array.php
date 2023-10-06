207260b36c5ec6eb57814a1c6ca9c8fa2b52fb2a
Angie <liya0706s@gmail.com>
2023-10-06 13:48:39 +0800

    array資料輸入

--- p20231006/array.php
+++ p20231006/array.php
@@ -8,3 +8,23 @@ $a[]=30;
 $a[]='泰山';
 $a[]='新莊';
 
+echo "<pre>";
+print_r($a);
+echo "</pre>";
+
+echo $a[0];
+echo "<br>";
+echo $a[3];
+
+$b=[];
+$b['姓名']='Angie';
+$b['最高學歷']='北商';
+$b['tel']='0925123456';
+$b['血型']='A';
+
+echo "<pre>";
+print_r($b);
+echo "</pre>";
+
+echo $b['姓名'];
+echo $b['血型'];
