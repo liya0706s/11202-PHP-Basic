192f53ac022d72793020319aa7bd38fd84f8922b
Angie <liya0706s@gmail.com>
2023-10-06 16:16:10 +0800

    pra更新

--- p20231006/pra.php
+++ p20231006/pra.php
@@ -1,10 +1,10 @@
 <h2>閏年判斷，給定一個西元年份，判斷是否為閏年</h2>
 <?php
-$year=1999;
+$year=104;
 echo "年份:" .$year;
 echo "<hr>";
 
-if($year%4==0 && $yeaar%100!=0) {
+if (($year%4==0 && $year%100!=0)||($year%100==0 && $year%400==0)) {
     echo $year. "是閏年";
 } else {
     echo $year. "是平年";
