365e0fb6f8711dd63ac710b872abd46eb3eb4d9b
Angie <liya0706s@gmail.com>
2023-10-13 11:31:04 +0800

    九九乘法表 交叉計算 簡易表

--- p20231013/p1013_pra02.php
+++ p20231013/p1013_pra02.php
@@ -30,6 +30,49 @@ for($i=1;$i<=9;$i++){
 echo "</table>";
 ?>
 
+<h2>交叉計算:九九乘法表</h2>
+<?php
+
+echo "<table>";
+echo "<tr>";
+for($j=0;$j<10;$j++){
+    // 在for裡面放if條件式判斷，讓第一列tr$j灰底色
+    if($j==0){
+        // 表頭是由0開始
+        echo "<tr style='background:#eee'>";
+    } else{
+        // 這邊是什麼意思?
+        echo "<tr>";
+    }
+
+    for($i=0;$i<10;$i++){
+        if($i==0){
+            echo "<td style='background:#eee'>";
+        } else{
+            // 這邊是什麼意思?
+            echo "<td>";
+        }
+
+        // 當$j等於零時，只印出$i
+        // 當$i等於零時，只印出$j
+        if($i==0 && $j==0){
+            echo "";
+        }elseif($j==0){
+            echo $i;
+        }elseif($i==0){
+            echo $j;
+        }else{
+            echo$j*$i;
+        }
+    }
+    echo "</tr>";
+}
+echo "</table>";
+?>
+
+
+
+
 <table>
     <tr>
         <td></td>
