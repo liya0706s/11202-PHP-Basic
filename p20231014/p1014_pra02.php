23f428b7d8cf2b3d2e831171817e01cd93bde8a0
Angie <liya0706s@gmail.com>
2023-10-14 14:34:21 +0800

    加上p20231013_mackliu_pra02有標頭的程式註解

--- p20231014/p1014_pra02.php
+++ p20231014/p1014_pra02.php
@@ -10,7 +10,7 @@
 </style>
 
 
-<h1>九九乘法表</h1>
+<h2>九九乘法表</h2>
 <?php
 // 步驟一:先列出九九乘法表的樣子
 // echo "1x1=1";
@@ -36,3 +36,66 @@ for($i=1;$i<=9;$i++){
     echo "</tr>";
 }
 echo "</table>";
+?>
+
+<h2>九九乘法表-有標頭</h2>
+<!-- <table>
+    <tr>
+        <td>1x1=</td>
+        <td>1x2</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>2x1</td>
+        <td>2x2</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>3x1</td>
+        <td>3x2</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>4x1</td>
+        <td>4x2</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+</table> -->
+    <!-- 想想看以上的迴圈 -->
+
+<?php
+echo "<table>";
+echo "<tr>";
+for($j=1;$j<=9;$j++){
+    echo "</tr>";
+    for($i=1;$i<=9;$i++){
+        echo "<td>";
+        echo $j*$i;
+        echo "</td>";
+    }
+}
+?>
