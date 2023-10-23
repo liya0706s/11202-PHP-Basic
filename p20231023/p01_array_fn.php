7c05372ea5aad827e03922401f1feb947f9bc1c3
Angie <liya0706s@gmail.com>
2023-10-23 09:26:11 +0800

    上課練習陣列常用函式

---
+++ p20231023/p01_array_fn.php
@@ -0,0 +1,32 @@
+<?php
+
+// $array=[3,2,7,10,30,17];
+
+// if(in_array(3,$array)){
+//     echo "數字有在陣列中";
+// } else {
+//     echo "數字不在陣列中";
+// }
+
+// //將索引另外取陣列列出來
+// echo "<pre>";
+// print_r(array_keys($array));
+// echo "</pre>";
+
+// echo "<pre>";
+// print_r($array);
+// echo "</pre>";
+
+// sort($array);
+
+
+$ss="today is a good day";
+$tt=explode(" ",$ss);
+print_r($tt);
+
+$cc=implode("--",$tt);
+echo $cc;
+
+ 
+
+
