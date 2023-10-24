3d388f2fa1ab1fbf7a2a46c19d247d55904a8a80
Angie <liya0706s@gmail.com>
2023-10-24 11:17:04 +0800

    連續五個周一的日期

--- p20231024/date.php
+++ p20231024/date.php
@@ -70,13 +70,46 @@ echo "<br>";
 echo date("Y-n-j G:i:s");
 echo "<br>";
 
-
-
 echo date("今天是西元Y年m月d日");
 // 上班日與否用平日和周末來判斷
-// 1(星期一)到7(星期天)
+// date("N") 1(星期一)到7(星期天)
 if(date("N")<=5){
     echo "上班日";
 }else{
     echo "假日";
 }
+?>
+
+<h2>利用迴圈來計算連續五個周一的日期</h2>
+<ul>
+<li>2021-10-04 星期一</li>
+<li>2021-10-11 星期一</li>
+<li>2021-10-18 星期一</li>
+<li>2021-10-25 星期一</li>
+<li>2021-11-01 星期一</li>
+</ul>
+
+<?php
+$todayWeek=date("N");
+// 代表星期
+// echo $todayWeek;
+// 今天是2週二，與下週一的距離是6 (7-2+1)
+// 這週一差一天，再算下週一
+// echo "<br>";
+// echo $todayWeek-1;
+// 距離週一差一天
+echo "<br>";
+$diff=1-$todayWeek;
+$lastMon=Strtotime("$diff days");
+$nextMon=date("Y-m-d", strtotime("+1 week", $lastMon));
+
+for($i=0;$i<5;$i++){
+    $nextMon=date("Y-m-d", strtotime("+1 week", strtotime($nextMon)));
+    // 每加一個week
+    echo $nextMon;
+    echo "<br>";
+}
+
+
+
+
