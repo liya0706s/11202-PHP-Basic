c6a1b5a305bc56a1374696ccff38c1b51262a726
Angie <liya0706s@gmail.com>
2023-10-24 14:08:27 +0800

    增加天數及週數的判斷

--- p20231024/calendar.php
+++ p20231024/calendar.php
@@ -6,13 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>線上月曆</title>
     <style>
-        table, th, tr, td {
+        table,
+        th,
+        tr,
+        td {
             border-collapse: collapse;
             border: 3px solid #999;
         }
 
         td {
-            border:1px solid #999;
+            border: 1px solid #999;
             border-collapse: collapse;
             padding: 5px 10px;
             /* padding抓 1:2或1:3 */
@@ -21,7 +24,67 @@
         }
     </style>
 </head>
+
 <body>
+
+    <?php
+    echo "<h3>";
+    echo date("西元Y年m月");
+    echo "</h3>";
+    // 程式產生西元年
+    $thisMonth = date("Y");
+    $thisFirstDay = date("Y-m-1");
+    $thisFirstDate = date('w', strtotime($thisFirstDay));
+    echo $thisFirstDate;
+    // 算幾個空白 目前星期和第一天的星期
+    // N or w? w比較符合現在,要小心處理
+    // strtotime($thisFirstDay) 第一天的秒數
+    // w:数字表示星期几	0（星期天）到 6（星期六）
+    $thisMonthDays = date("t");
+    $thisLastDay = date("Y-m-$thisMonthDays");
+    // t:該月份的天數
+    echo "<br>";
+    $weeks=ceil(($thisMonthDays+$thisFirstDate)/7);
+    echo "這個月共有". $weeks. "週";
+    
+    echo "<br>";
+    echo "這個月共有" . $thisMonthDays . "天";
+    echo $thisLastDay;
+
+
+    echo "<table>";
+    echo "<tr>";
+    echo "<td>日</td>";
+    echo "<td>一</td>";
+    echo "<td>二</td>";
+    echo "<td>三</td>";
+    echo "<td>四</td>";
+    echo "<td>五</td>";
+    echo "<td>六</td>";
+    echo "</tr>";
+
+    for ($i = 0; $i < $weeks ; $i++) {
+        echo "<tr>";
+        for ($j = 0; $j < 7; $j++) {
+            echo "<td>";
+            $tmp = 7 * ($i + 1) - (6 - $j) - $thisFirstDate;
+            if ($tmp > 0 && $tmp<=$thisMonthDays) {
+                echo $tmp;
+            }
+
+            // $i=0 要加一，否則乘以會有問題
+            // $j是星期, $i是週
+            echo "</td>";
+        }
+        echo "</tr>";
+    }
+
+    echo "</table>";
+
+
+    ?>
+
+
     <h3>西元2023年10月</h3>
     <table>
         <tr>
