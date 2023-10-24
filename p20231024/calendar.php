07673365a6ffc6d32d7af7422b0d9e65979d8dd0
Angie <liya0706s@gmail.com>
2023-10-24 11:36:22 +0800

    月曆

---
+++ p20231024/calendar.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>線上月曆</title>
+    <style>
+        table, th, tr, td {
+            border-collapse: collapse;
+            border: 3px solid #999;
+        }
+
+        td {
+            border:1px solid #999;
+            border-collapse: collapse;
+            padding: 5px 10px;
+            /* padding抓 1:2或1:3 */
+            text-align: center;
+            /* emmet:tac+tab */
+        }
+    </style>
+</head>
+<body>
+    <h3>西元2023年10月</h3>
+    <table>
+        <tr>
+            <th>日</th>
+            <th>一</th>
+            <th>二</th>
+            <th>三</th>
+            <th>四</th>
+            <th>五</th>
+            <th>六</th>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>4</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>6</td>
+            <td>7</td>
+            <td>8</td>
+            <td>9</td>
+            <td>10</td>
+            <td>11</td>
+        </tr>
+        <tr>
+            <td>12</td>
+            <td>13</td>
+            <td>14</td>
+            <td>15</td>
+            <td>16</td>
+            <td>17</td>
+            <td>18</td>
+        </tr>
+        <tr>
+            <td>19</td>
+            <td>20</td>
+            <td>21</td>
+            <td>22</td>
+            <td>23</td>
+            <td>24</td>
+            <td>25</td>
+        </tr>
+        <tr>
+            <td>26</td>
+            <td>27</td>
+            <td>28</td>
+            <td>29</td>
+            <td>30</td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+</body>
+
+</html>
